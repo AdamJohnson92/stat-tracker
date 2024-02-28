5eab2899148c79f38085de2e128cc35051152403
AdamJohnson92 <127777953+AdamJohnson92@users.noreply.github.com>
2024-02-28 13:01:42 -0700

    Messed around with boostrap styling to find which styles can use and which to use custom.

--- stat-tracker/src/pages/StatsFormPage.js
+++ stat-tracker/src/pages/StatsFormPage.js
@@ -10,14 +10,14 @@ const StatsFormPage = () => {
         <>
             <h2 className="padding-top">New Stats Form</h2>
             <form className="padding-top column container">
-                <div className="container">
+                <div className="form-group">
                     <select className="col-6">
                         <option>A-Jo</option>
                         <option>annathekate14</option>
                     </select>
                 </div>
 
-                <button className="col-2">Button</button>
+                <button className="col-4 btn margin">Button</button>
             </form>
         </>
 
